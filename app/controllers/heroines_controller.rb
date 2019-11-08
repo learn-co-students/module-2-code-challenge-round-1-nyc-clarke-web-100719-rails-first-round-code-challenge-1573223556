622a292c4a8a05df56dbbe4dd5e21830a9d647d3
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-11-08 10:10:37 -0500

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show]
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to @heroine
+    else 
+      flash[:messages] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)  
+  end
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
   end
 end
