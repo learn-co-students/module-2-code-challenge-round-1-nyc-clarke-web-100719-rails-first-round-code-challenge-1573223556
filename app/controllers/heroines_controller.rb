b12d1724aa02ecef5ec40de64cdd407233a41954
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-08 10:40:38 -0500

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,57 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
     @heroines = Heroine.all
+    render :index
   end
+
+  def new
+    @heroine = Heroine.new
+    render :new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:messages] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+    render :show
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:messages] = @heroine.errors.full_messages
+      redirect_to edit_heroine_path
+    end
+  end
+
+  def destroy
+    @heroine.destroy
+    redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,56 @@
 class PowersController < ApplicationController
+  before_action :set_power, only: [:show, :edit, :update, :destroy]
+
   def index
     @powers = Power.all
+    render :index
+  end
+
+  def new
+    @power = Power.new
+    render :new
+  end
+
+  def create
+    @power = Power.create(power_params)
+
+    if @power.valid?
+      redirect_to power_path(@power)
+    else
+      flash[:messages] = @power.errors.full_messages
+      redirect_to new_power_path
+    end
+  end
+
+  def show
+    render :show
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    if @power.update(power_params)
+      redirect_to power_path(@power)
+    else
+      flash[:messages] = @power.errors.full_messages
+      redirect_to edit_power_path
+    end
+  end
+
+  def destroy
+    @power.destroy
+    redirect_to powers_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
+  def set_power
+    @power = Power.find(params[:id])
   end
 end
